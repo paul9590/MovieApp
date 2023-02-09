c46424e6cebd8d8d786d840240ab608f8f1ae2d5
paul9590 <paul95901234@gmail.com>
2023-02-10 00:23:26 +0900

    Design UI

---
+++ app/src/main/java/com/example/movieapp/model/Movie.kt
@@ -0,0 +1,3 @@
+package com.example.movieapp.model
+
+data class Movie(val title: String, val date: String, val rating: String, val url: String)
---
+++ app/src/main/java/com/example/movieapp/vm/MovieViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.movieapp.vm
+
+class MovieViewModel {
+}
